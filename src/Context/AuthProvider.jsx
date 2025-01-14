09218a86fc407414eb2300ed4389d23e3ce941b3
Hayder Ali <hayderbd4290@gmail.com>
2025-01-14 21:49:52 +0600

    auth provider context api created

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,22 +1,78 @@
-import { createContext, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { auth } from "../firebase/firebase.config";
 
-const AuthContext = createContext(null)
+const AuthContext = createContext(null);
 
-const AuthProvider = ({children}) => {
-    const [user, setUser] = useState(null)
-    const [loading, setLoading] = useState(false)
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const googleProvider = new GoogleAuthProvider();
 
+  const registerUser = async (email, password) => {
+    setLoading(true);
+    return await createUserWithEmailAndPassword(auth, email, password);
+  };
 
-    const authInfo ={
-       user, 
-       loading,
+  const updateUser = async (name, photoPath) => {
+    return await updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photoPath,
+    });
+  };
 
+  const loginUser = async (email, password) => {
+    setLoading(true);
+    return await signInWithEmailAndPassword(email, password);
+  };
+
+  const googleLogin = async()=>{
+    setLoading(true)
+    return await signInWithPopup(auth, googleProvider)
+  }
+
+//   get current User
+  useEffect(()=>{
+    const unSubscribe = onAuthStateChanged(auth,(currentUser)=>{
+        setUser(currentUser)
+        setLoading(false)
+    })
+
+    return ()=>{
+        unSubscribe()
     }
-    return (
-        <AuthContext.Provider value={authInfo}>
-            {children}
-        </AuthContext.Provider>
-    );
+  },[])
+
+  const logOutUser = async()=>{
+    setLoading(false)
+    return await signOut(auth)
+  }
+
+
+  const authInfo = {
+    user,
+    loading,
+    registerUser,
+    updateUser,
+    loginUser,
+    googleLogin,
+    logOutUser
+
+  };
+
+  console.log(user)
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
