cf89c0254d2a17a5a3775d200f42abdcd79ff163
Hayder Ali <hayderbd4290@gmail.com>
2025-01-20 00:59:16 +0600

    navbar sm devices menu created

--- src/Components/Shared/NavBar.jsx
+++ src/Components/Shared/NavBar.jsx
@@ -11,10 +11,13 @@ import { FaPerson } from "react-icons/fa6";
 import { FiLogIn } from "react-icons/fi";
 import { NavLink, useNavigate } from "react-router";
 import useAuth from "../../Context/useAuth";
+import { RiMenuFold2Fill } from "react-icons/ri";
+import { VscChromeClose } from "react-icons/vsc";
 
 const NavBar = () => {
   const navigate = useNavigate();
   const { user, loading } = useAuth();
+  const [menu, setMenu] = useState(false)
 
   const [lang, setLang] = useState(true);
   const { t, i18n } = useTranslation();
@@ -25,7 +28,7 @@ const NavBar = () => {
   };
 
   return (
-    <div className="flex py-3 sticky top-0 z-10 bg-slate-50 bg-opacity-60 backdrop-blur-md px-2 md:px-6 justify-between items-center">
+    <div className="flex py-3  sticky top-0 z-40 bg-slate-50 bg-opacity-60 backdrop-blur-md px-2 md:px-6 justify-between items-center">
       {/* img */}
       <div className="">
         <div className="flex justify-center items-center gap-3">
@@ -40,7 +43,7 @@ const NavBar = () => {
         </div>
       </div>
       {/* menu */}
-      <div className="flex justify-center items-center gap-3 md:gap-6">
+      <div className="flex  justify-center items-center gap-3 md:gap-6">
         {/* menuitem desktop*/}
         <div className="hidden lg:flex ">
           <ul className="flex font-medium cursor-pointer justify-center items-center gap-6">
@@ -50,19 +53,25 @@ const NavBar = () => {
                 {t("menu1")}
               </li>
             </NavLink>
-            <NavLink to={'/bioData'}><li className="flex items-center justify-center gap-1">
-              {" "}
-              <FaPerson />
-              {t("menu2")}
-            </li></NavLink>
-            <NavLink to={'/about'}><li className="flex items-center justify-center gap-1">
-              <BsInfoCircleFill />
-              {t("menu3")}
-            </li></NavLink>
-            <NavLink to={'/contact'}><li className="flex items-center justify-center gap-1">
-              <IoMdContact />
-              {t("menu4")}
-            </li></NavLink>
+            <NavLink to={"/bioData"}>
+              <li className="flex items-center justify-center gap-1">
+                {" "}
+                <FaPerson />
+                {t("menu2")}
+              </li>
+            </NavLink>
+            <NavLink to={"/about"}>
+              <li className="flex items-center justify-center gap-1">
+                <BsInfoCircleFill />
+                {t("menu3")}
+              </li>
+            </NavLink>
+            <NavLink to={"/contact"}>
+              <li className="flex items-center justify-center gap-1">
+                <IoMdContact />
+                {t("menu4")}
+              </li>
+            </NavLink>
             {user && (
               <NavLink to="dashboard">
                 <li className="flex items-center justify-center gap-1">
@@ -73,6 +82,54 @@ const NavBar = () => {
             )}
           </ul>
         </div>
+
+        <button 
+        onClick={()=>setMenu(!menu)}
+        className="text-4xl flex lg:hidden cursor-pointer">
+         {!menu?<RiMenuFold2Fill />:<VscChromeClose />}
+        </button>
+
+        {/* menuitem mobile*/}
+        <div className={`${menu?"flex":"hidden"} bg-blue-200 relative duration-500 lg:hidden `}>
+          <ul 
+          onClick={()=>setMenu(!menu)}
+          className={`flex py-6 absolute right-0 px-20 duration-500 z-10 ${menu?"top-12":"-top-[1452px]"}   flex-col bg-blue-200 font-medium cursor-pointer justify-center items-center gap-6`}>
+            <NavLink to="/">
+              <li className="flex items-center justify-center gap-1">
+                <IoHome />
+                {t("menu1")}
+              </li>
+            </NavLink>
+            <NavLink to={"/bioData"}>
+              <li className="flex items-center justify-center gap-1">
+                {" "}
+                <FaPerson />
+                {t("menu2")}
+              </li>
+            </NavLink>
+            <NavLink to={"/about"}>
+              <li className="flex items-center justify-center gap-1">
+                <BsInfoCircleFill />
+                {t("menu3")}
+              </li>
+            </NavLink>
+            <NavLink to={"/contact"}>
+              <li className="flex items-center justify-center gap-1">
+                <IoMdContact />
+                {t("menu4")}
+              </li>
+            </NavLink>
+            {user && (
+              <NavLink to="dashboard">
+                <li className="flex items-center justify-center gap-1">
+                  <MdDashboard />
+                  {t("menu5")}
+                </li>
+              </NavLink>
+            )}
+          </ul>
+        </div>
+
         {loading ? (
           ""
         ) : (
