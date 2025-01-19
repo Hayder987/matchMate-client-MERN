994a53cbbf4aafc1492bbbdb3d8b87ffdfada02b
Hayder Ali <hayderbd4290@gmail.com>
2025-01-20 03:30:56 +0600

    succcess section done

--- src/Components/HomeComponents/SuccessCounter.jsx
+++ src/Components/HomeComponents/SuccessCounter.jsx
@@ -6,6 +6,7 @@ import female from "../../assets/logo/women.png";
 import merrige from "../../assets/logo/merriage.png";
 import SectionTitle from "../commonComponents/SectionTitle";
 import { useTranslation } from "react-i18next";
+import LoaderSpinner from "../commonComponents/LoaderSpinner";
 
 const SuccessCounter = () => {
   const serverUrl = useAxiosPublic();
@@ -25,7 +26,78 @@ const SuccessCounter = () => {
         title={t("successTitle")}
         desc={t("successDesc")}
       ></SectionTitle>
-      <div className="bg-blue-900 py-24 lg:py-40 px-2"></div>
+      <div className="bg-blue-800 py-24 lg:py-28 px-2">
+        {publicLoading ? (
+          <LoaderSpinner></LoaderSpinner>
+        ) : (
+          <div className="container grid px-4 md:px-8 lg:px-14 gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mx-auto">
+            {/* 1 */}
+            <div className="flex gap-6 justify-center items-center">
+              <div className=" ">
+                <div className="bg-blue-700 w-32 h-32  p-6 rounded-full">
+                  <img src={bio} alt="" className="w-20 h-20" />
+                </div>
+              </div>
+              <div className="">
+                <h1 className="text-6xl mb-2 text-white font-bold">
+                  {publicInfo?.totalBio}+
+                </h1>
+                <p className="text-pink-400 font-semibold text-2xl">
+                  Total Bio
+                </p>
+              </div>
+            </div>
+            {/* 2 */}
+            <div className="flex gap-6 justify-center items-center">
+              <div className=" ">
+                <div className="bg-blue-700 w-32 h-32  p-6 rounded-full">
+                  <img src={female} alt="" className="w-20 h-20" />
+                </div>
+              </div>
+              <div className="">
+                <h1 className="text-6xl mb-2 text-white font-bold">
+                {publicInfo?.female}
+                </h1>
+                <p className="text-pink-400 font-semibold text-2xl">
+                  Female
+                </p>
+              </div>
+            </div>
+            {/* 3 */}
+            <div className="flex gap-6 justify-center items-center">
+              <div className=" ">
+                <div className="bg-blue-700 w-32 h-32  p-6 rounded-full">
+                  <img src={male} alt="" className="w-20 h-20" />
+                </div>
+              </div>
+              <div className="">
+                <h1 className="text-6xl mb-2 text-white font-bold">
+                  {publicInfo?.male}
+                </h1>
+                <p className="text-pink-400 font-semibold text-2xl">
+                  Male
+                </p>
+              </div>
+            </div>
+            {/* 4 */}
+            <div className="flex gap-6 justify-center items-center">
+              <div className=" ">
+                <div className="bg-blue-700 w-32 h-32  p-6 rounded-full">
+                  <img src={merrige} alt="" className="w-20 h-20" />
+                </div>
+              </div>
+              <div className="">
+                <h1 className="text-6xl mb-2 text-white font-bold">
+                  {publicInfo?.marriage}+
+                </h1>
+                <p className="text-pink-400 font-semibold text-2xl">
+                  Marriage
+                </p>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
