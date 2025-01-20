2de21362b2630c64408a5fdb6908d0bcf1e0ad54
Hayder Ali <hayderbd4290@gmail.com>
2025-01-20 17:52:56 +0600

    about section done

--- src/Components/HomeComponents/WorkSectionCard.jsx
+++ src/Components/HomeComponents/WorkSectionCard.jsx
@@ -1,17 +1,17 @@
-
-
-const WorkSectionCard = ({logo, title, desc}) => {
-    return (
-        <div>
-            <div className="flex p-8 min-h-[480px]  rounded-md bg-white shadow-lg flex-col justify-center items-center">
-                <img src={logo} alt="" className="mb-6" />
-                <div className="flex-1">
-                <h1 className="text-center text-2xl font-bold mb-4">{title}</h1>
-                <p className="text-center  text-gray-600 font-medium">{desc}</p>
-                </div>
-            </div>
+const WorkSectionCard = ({ logo, title, desc }) => {
+  return (
+    <div>
+      <div className="flex p-8 rounded-md bg-white shadow-lg flex-col justify-center items-center h-full">
+        <div className="flex-grow flex justify-center items-center">
+          <img src={logo} alt="" className="mb-6 max-h-20" />
         </div>
-    );
+        <div className="flex flex-col flex-grow">
+          <h1 className="text-center text-2xl font-bold mb-4">{title}</h1>
+          <p className="text-center text-gray-600 font-medium">{desc}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default WorkSectionCard;
--- src/Pages/About/AboutPage.jsx
+++ src/Pages/About/AboutPage.jsx
@@ -1,9 +1,46 @@
+import { useTranslation } from "react-i18next";
+import Banner from "../../assets/images/about Banner.jpg";
+
 const AboutPage = () => {
-    return (
-        <div>
-            AboutPage Component
+    const { t } = useTranslation();
+  return (
+    <div
+      style={{
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+          url(${Banner})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundAttachment: "fixed",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+      className="w-full flex justify-center p-8 md:p-14 lg:p-24 items-center min-h-screen"
+    >
+      <div className="bg-slate-100 flex gap-10 flex-col lg:flex-row container mx-auto">
+        {/* img */}
+        <div className="lg:w-1/2">
+          <img src={Banner} alt="" className=" w-full h-full" />
         </div>
-    );
+        {/* text */}
+        <div className="lg:w-1/2 px-3 py-6">
+          <h1 className="text-3xl font-bold mb-4">{t('aboutTitle')}</h1>
+          <p className="text-gray-600 font-medium text-xl mb-10">
+            {t('aboutDesc')}
+          </p>
+          <h3 className="text-2xl font-bold mb-6">{t('aboutWhyTitle')}</h3>
+          <ul className="list-disc flex flex-col gap-2 px-8 font-medium text-gray-800">
+            <li className="">{t('aboutLi1')}</li>
+            <li className="">{t('aboutLi2')}</li>
+            <li className="">{t('aboutLi3')}</li>
+            <li className="">{t('aboutLi4')}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
