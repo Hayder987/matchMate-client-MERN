db4745086a53776bba1b676e103ecae8b76622d3
Hayder Ali <hayderbd4290@gmail.com>
2025-01-20 18:39:03 +0600

    contact us section done

--- src/Pages/About/AboutPage.jsx
+++ src/Pages/About/AboutPage.jsx
@@ -25,7 +25,7 @@ const AboutPage = () => {
           <img src={Banner} alt="" className=" w-full h-full" />
         </div>
         {/* text */}
-        <div className="lg:w-1/2 px-3 py-6">
+        <div className="lg:w-1/2 px-3 py-10">
           <h1 className="text-3xl font-bold mb-4">{t('aboutTitle')}</h1>
           <p className="text-gray-600 font-medium text-xl mb-10">
             {t('aboutDesc')}
--- src/Pages/contactPage/ContactPage.jsx
+++ src/Pages/contactPage/ContactPage.jsx
@@ -1,9 +1,58 @@
+import { useTranslation } from "react-i18next";
+import contactBannner from "../../assets/images/contactUs.jpg";
+
 const ContactPage = () => {
-    return (
-        <div>
-            ContactPage Component
+  const { t } = useTranslation();
+  return (
+    <div
+      style={{
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+              url(${contactBannner})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundAttachment: "fixed",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+      className="w-full flex justify-center p-8 md:p-14 lg:p-24 items-center min-h-screen"
+    >
+      <div className="bg-slate-100 py-20 flex gap-10 flex-col lg:flex-row container mx-auto">
+        {/* img */}
+        <div className="lg:w-1/2">
+          <img src={contactBannner} alt="" className=" w-full h-full" />
         </div>
-    );
+        {/* text */}
+        <div className="lg:w-1/2 px-3 py-10">
+          <h1 className="text-3xl font-bold mb-4">{t("contactTitle")}</h1>
+          <p className="text-gray-600 font-medium text-xl mb-10">
+            {t("contactDes")}
+          </p>
+          <h3 className="text-2xl font-bold mb-6">{t("contactme")}</h3>
+          <div className="">
+            <div className="mb-4">
+              <p className="text-xl font-bold mb-3">📍 {t("con1")}</p>
+              <p className="flex flex-col px-6">
+                <span className="">MatchMate HQ</span>
+                <span className="">123 Love Street</span>
+                <span className="">Dhaka, Bangladesh</span>
+              </p>
+            </div>
+            <div className="mb-4">
+              <p className="text-xl font-bold mb-3">📞 {t('con2')}</p>
+              <p className="px-6">+880 1771 814597</p>
+            </div>
+            <div className="">
+                <p className="text-xl font-bold mb-3">📧 {t('con3')}</p>
+                <p className="px-6">hayderbd4290@gmail.com</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ContactPage;
