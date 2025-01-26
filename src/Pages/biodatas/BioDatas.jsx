767525fd31d4dc84ce6eac7e568d9e4cdcd0ff4f
Hayder Ali <hayderbd4290@gmail.com>
2025-01-26 23:45:40 +0600

    change repo

--- src/Pages/biodatas/BioDatas.jsx
+++ src/Pages/biodatas/BioDatas.jsx
@@ -27,10 +27,13 @@ const BioDatas = () => {
   const [ageValue, setAgeValue] = useState(0);
   const [biodataType, setBiodataType] = useState("");
   const [division, setDivision] = useState("");
-  const [sideMenu, setSideMenu] = useState(false)
+  const [sideMenu, setSideMenu] = useState(false);
 
-
-  const { data: allBioData = [], isLoading: allBioLoading, refetch:AllBioDataFetch } = useQuery({
+  const {
+    data: allBioData = [],
+    isLoading: allBioLoading,
+    refetch: AllBioDataFetch,
+  } = useQuery({
     queryKey: ["allBioData", page, ageValue, biodataType, division],
     queryFn: async () => {
       const { data } = await serverUrl(
@@ -41,13 +44,13 @@ const BioDatas = () => {
     keepPreviousData: true,
   });
 
-const resetHandler = async()=>{
-  setAgeValue(0)
-  setBiodataType('')
-  setDivision('')
+  const resetHandler = async () => {
+    setAgeValue(0);
+    setBiodataType("");
+    setDivision("");
 
-  AllBioDataFetch()
-}
+    AllBioDataFetch();
+  };
 
   return (
     <div>
@@ -65,7 +68,7 @@ const resetHandler = async()=>{
             {/* age */}
             <div className="">
               <p className="text-xl flex justify-between mb-6 font-semibold">
-              Find Ages Above
+                Find Ages Above
                 <span className="">{ageValue}</span>
               </p>
               <input
@@ -114,23 +117,36 @@ const resetHandler = async()=>{
             </div>
             {/* reset  */}
             <div className="mt-12">
-                <button 
+              <button
                 onClick={resetHandler}
-                className="bg-blue-800 hover:bg-blue-400 hover:text-black font-semibold w-full py-2 text-white px-6">Reset</button>
+                className="bg-blue-800 hover:bg-blue-400 hover:text-black font-semibold w-full py-2 text-white px-6"
+              >
+                Reset
+              </button>
             </div>
           </div>
         </div>
 
-        <div className={`flex duration-500 fixed z-20 ${!sideMenu?'left-2':"left-[250px]"} lg:hidden`}>
-            <button 
-            onClick={()=>setSideMenu(!sideMenu)}
-            className="text-4xl bg-blue-200  "><TfiLayoutSidebarRight /></button>
+        <div
+          className={`flex duration-500 fixed z-20 ${
+            !sideMenu ? "left-2" : "left-[250px]"
+          } lg:hidden`}
+        >
+          <button
+            onClick={() => setSideMenu(!sideMenu)}
+            className="text-4xl bg-blue-200  "
+          >
+            <TfiLayoutSidebarRight />
+          </button>
         </div>
-      
+
         {/* aside mobile*/}
-        <div 
-        onBlur={()=>setSideMenu(false)}
-        className={`w-8/12 duration-500 absolute ${!sideMenu?'-left-[1400px]':"left-2"} z-10 flex flex-col lg:hidden  py-6 px-4 h-full min-h-[90vh] bg-slate-100`}>
+        <div
+          onBlur={() => setSideMenu(false)}
+          className={`w-8/12 duration-500 absolute ${
+            !sideMenu ? "-left-[1400px]" : "left-2"
+          } z-10 flex flex-col lg:hidden  py-6 px-4 h-full min-h-[90vh] bg-slate-100`}
+        >
           <h1 className="flex mb-20  font-bold text-blue-800 text-2xl items-center gap-2">
             <IoFilterSharp />
             Filter Bio
@@ -139,7 +155,7 @@ const resetHandler = async()=>{
             {/* age */}
             <div className="">
               <p className="text-xl flex justify-between mb-6 font-semibold">
-              Find Ages Above
+                Find Ages Above
                 <span className="">{ageValue}</span>
               </p>
               <input
@@ -188,13 +204,15 @@ const resetHandler = async()=>{
             </div>
             {/* reset  */}
             <div className="mt-12">
-                <button 
+              <button
                 onClick={resetHandler}
-                className="bg-blue-800 hover:bg-blue-400 hover:text-black font-semibold w-full py-2 text-white px-6">Reset</button>
+                className="bg-blue-800 hover:bg-blue-400 hover:text-black font-semibold w-full py-2 text-white px-6"
+              >
+                Reset
+              </button>
             </div>
           </div>
         </div>
-
 
         {/* card */}
         <div className="lg:w-10/12  p-6  bg-white">
@@ -203,7 +221,34 @@ const resetHandler = async()=>{
           ) : (
             <div className="">
               {allBioData.items?.length === 0 ? (
-                <NoData></NoData>
+                <div>
+                  <div className="min-h-[90vh]">
+                  <NoData></NoData>
+                  </div>
+                  <div className="flex justify-center space-x-4">
+                    <button
+                      onClick={() => setPage((prev) => Math.max(prev - 1, 1))}
+                      disabled={page === 1}
+                      className="px-4 py-2 bg-blue-800 text-white rounded disabled:opacity-50"
+                    >
+                      Previous
+                    </button>
+                    <span className="px-4 py-2">
+                      Page {page} of {allBioData.totalPages}
+                    </span>
+                    <button
+                      onClick={() =>
+                        setPage((prev) =>
+                          prev < allBioData.totalPages ? prev + 1 : prev
+                        )
+                      }
+                      disabled={page === allBioData.totalPages}
+                      className="px-8 py-2 bg-blue-800 text-white rounded disabled:opacity-50"
+                    >
+                      Next
+                    </button>
+                  </div>
+                </div>
               ) : (
                 <div className="min-h-[90vh] flex flex-col justify-between p-4">
                   <div className="grid gap-8 mb-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
